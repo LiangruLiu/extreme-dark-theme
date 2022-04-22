fcf42a27f64eb535d8cfc7e0a5d98f35fbde595c
123 <456>
2022-04-22 17:24:45 +0800

    0.6.1

--- sources/extension.js
+++ sources/extension.js
@@ -1,13 +1,24 @@
 const vscode = require ("vscode")  // 请忽略提示，千万不要点击自动修复
-const config = vscode.workspace.getConfiguration()
 const feature = require ("./feature.js")
 
+
 exports.activate = function (context) {
-	context.subscriptions.push (vscode.commands.registerCommand ("extension.HiLLR", function () {
+	context.subscriptions.push( vscode.commands.registerCommand( "extension.HiLLR", function () {
 		vscode.window.showInformationMessage ("Hello, LLR!")
 	}))
-	feature.toggleTokenColors (config.get ("Extreme-Dark-Theme.Token Colors"))
+
+	// var getCfg = vscode.workspace.getConfiguration ("Extreme-Dark-Theme").get
+	// feature.toggleTokenColors (getCfg("Token Colors"))
+
+	context.subscriptions.push( vscode.workspace.onDidChangeConfiguration( (event) => {
+		var getCfg = vscode.workspace.getConfiguration ("Extreme-Dark-Theme").get
+		if (event.affectsConfiguration ("Extreme-Dark-Theme.Token Colors")) {
+			feature.toggleTokenColors (getCfg("Token Colors"))
+			feature.promptToReload ()
+		}
+	}))
 }
+
 
 exports.deactivate = function () {
 }
--- sources/feature.js
+++ sources/feature.js
@@ -1,34 +1,44 @@
+const vscode = require ("vscode")  // 请忽略提示，千万不要点击自动修复
 const fs = require ("fs")  // 请忽略提示，千万不要点击自动修复
 const path = require ("path")
+
 
 function overtype (filePath, cursorPos, content) {
 	var writer = fs.createWriteStream (filePath, {flags: "r+", start: cursorPos})
 	// 此函数不能用try-catch，而要用：
-	writer.on ("error", function (err) {
-			console.log (err)
-			writer.end ()
-		})
+	writer.on( "error", function (err) {
+		console.log (err)
+		writer.end ()
+	})
 	// 但其实不做错误处理也行，因为VSCode插件系统会忽略错误继续执行
 	writer.write (content, "utf8")
 	writer.end ()
 }
 
 // 本函数通过切换注释实现
-function toggleTokenColors (colorSet) {
+exports.toggleTokenColors = function (colorSet) {
 	themeFilePath = path.join (__dirname, "../themes/ExtremeDark.json")  // 用__dirname获取当前模块的目录名
 	if (colorSet === "OneDarkPro") {
 		overtype (themeFilePath, 103, "//")
 		overtype (themeFilePath, 148, "  ")
-	}
-	else if (colorSet === "Original") {
+	} else if (colorSet === "Original") {
 		overtype (themeFilePath, 103, "  ")
 		overtype (themeFilePath, 148, "//")
-	}
-	else {
+	} else {
 		console.log ("Name Error!")
 	}
 }
-exports.toggleTokenColors = toggleTokenColors
+
+
+exports.promptToReload = function () {
+	const message = "Changed. Please restart VSCode to apply."
+	const action = ["Reload", "Not Now"]
+	vscode.window.showInformationMessage (message, ...action) .then( (selectedAction) => {
+		if (selectedAction === "Reload") {
+			vscode.commands.executeCommand ("workbench.action.reloadWindow");
+		}
+	})
+}
 
 
 // 似乎对ts文件支持不够好
